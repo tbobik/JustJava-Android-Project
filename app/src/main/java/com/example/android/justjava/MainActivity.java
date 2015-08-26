f355d839c8b7d43902f4e3648fccbf55e2368f7f
tbobik <tbobik91@gmail.com>
2015-08-26 13:42:59 -0700

    JustJava Update

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.justjava;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 /**
@@ -23,22 +24,29 @@ public class MainActivity extends ActionBarActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = chocolateCheckBox.isChecked();
         int price = calculatePrice();
-        displayMessage(createOrderSummary(price));
+        displayMessage(createOrderSummary(price, hasWhippedCream, hasChocolate));
 
     }
 
     /**
      * This method returns order summary.
      *  @param price of the order
+     *   @param addWhippedCream is whether or not the user wants whipped cream topping
      *  @return text summary
      */
-    private String createOrderSummary(int price)
+    private String createOrderSummary(int price, boolean addWhippedCream, boolean addChocolate)
     {
         String priceMessage = "Name: Kaptin Kunal";
+        priceMessage += "\nAdd whipped cream?" + addWhippedCream;
+        priceMessage += "\nAdd chocolate?" + addChocolate;
         priceMessage += "\nQuantity: " + quantity;
         priceMessage += "\nTotal: #" + price;
-        priceMessage +=  "\nThank you!";
+        priceMessage += "\nThank you!";
         return(priceMessage);
     }
 
