00c210013ffe2385637e8cf0e48999a023c11685
tbobik <tbobik91@gmail.com>
2015-08-26 22:13:05 -0700

    JustJava Android Update

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 /**
@@ -24,49 +25,65 @@ public class MainActivity extends ActionBarActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
+        // Obtain the name of the user
+        EditText nameField= (EditText) findViewById(R.id.name_field);
+        String name = nameField.getText().toString();
+
+        // Figure out if the user wants whipped cream topping
         CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
         boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+
+        // Figure out if the user wants chocolate topping
         CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
         boolean hasChocolate = chocolateCheckBox.isChecked();
-        int price = calculatePrice();
-        displayMessage(createOrderSummary(price, hasWhippedCream, hasChocolate));
+
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
+        displayMessage(createOrderSummary(name, price, hasWhippedCream, hasChocolate));
 
     }
 
+
     /**
      * This method returns order summary.
+     *  @param name of the customer
      *  @param price of the order
-     *   @param addWhippedCream is whether or not the user wants whipped cream topping
+     *  @param addWhippedCream is whether or not the user wants whipped cream topping
      *  @return text summary
      */
-    private String createOrderSummary(int price, boolean addWhippedCream, boolean addChocolate)
+    private String createOrderSummary(String name, int price, boolean addWhippedCream, boolean addChocolate)
     {
-        String priceMessage = "Name: Kaptin Kunal";
+        String priceMessage = "Name: " + name;
         priceMessage += "\nAdd whipped cream?" + addWhippedCream;
         priceMessage += "\nAdd chocolate?" + addChocolate;
         priceMessage += "\nQuantity: " + quantity;
-        priceMessage += "\nTotal: #" + price;
+        priceMessage += "\nTotal: " + price;
         priceMessage += "\nThank you!";
         return(priceMessage);
     }
 
     /**
      * Calculates the price of the order.
+     * @param addWhippedCream is whether or not the user wants whipped cream topping
+     * @param addChocolate is whether or not the user wants chocolate topping
      * @return total price
      */
-    private int calculatePrice() {
-        return  quantity * 5;
-    }
+    private int calculatePrice(boolean addWhippedCream, boolean addChocolate) {
+        // Price of 1 cup of coffee
+        int basePrice = 5;
 
-    /**
-     * This method displays the given quantity value on the screen.
-     */
-    private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(
-                R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
-    }
+        // Add $1 if the user wants whipped cream
+        if (addWhippedCream) {
+            basePrice += 1;
+        }
 
+        // Add $2 if the user wants chocolate
+        if (addChocolate){
+            basePrice += 2;
+        }
+
+        // Calculate the total order price by multiplying quantity * basePrice
+        return  quantity * basePrice;
+    }
 
     /**
      * This method displays the given text on the screen.
@@ -77,18 +94,33 @@ public class MainActivity extends ActionBarActivity {
     }
 
     /**
-     * This method is called when the + button is clicked.
+     * This method is called when the plus button is clicked.
      */
     public void increment(View view) {
+        if (quantity == 100) {
+            return;
+        }
         quantity = quantity + 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     /**
-     * This method is called when the + button is clicked.
+     * This method is called when the minus button is clicked.
      */
     public void decrement(View view) {
+        if (quantity == 0) {
+            return;
+        }
         quantity = quantity - 1;
-        display(quantity);
+        displayQuantity(quantity);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void displayQuantity(int numberOfCoffees) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText("" + numberOfCoffees);
     }
 }
